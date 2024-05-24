51f433ece27c5ceb4425ea628ddcd00905738ca4
rupesh00mahat <rupesh00mahat@gmail.com>
2024-05-24 16:06:59 +0545

    change layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,46 +6,20 @@
  */
 
 import * as React from "react"
-import { useStaticQuery, graphql } from "gatsby"
+// import {  graphql } from "gatsby"
+import Header from './header/header';
+import OurServices from './our-services/our-services';
+import Navbar from './header/navbar';
 
-import Header from "./header"
-import "./layout.css"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: `var(--size-content)`,
-          padding: `var(--size-gutter)`,
-        }}
-      >
-        <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `var(--space-5)`,
-            fontSize: `var(--font-sm)`,
-          }}
-        >
-          © {new Date().getFullYear()} &middot; Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  )
+ return(
+  <div>
+    <Header/>
+    <OurServices/>
+    <Navbar/>
+  </div>
+ );
 }
 
 export default Layout
