44a0c2bb7038e5924dbc3ce2e31a2cfd0975bf4c
rozeensubedi <59768755+rozeensubedi@users.noreply.github.com>
2024-07-30 11:22:34 +0545

    typo

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,8 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
- */
-
-/**
- * @type {import('gatsby').GatsbyConfig}
- */
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
@@ -21,6 +12,7 @@ module.exports = {
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
+        fastHash: true,
       },
     },
     {
@@ -28,20 +20,15 @@ module.exports = {
       options: {
         name: `about-us`,
         path: `${__dirname}/content/about-us`,
+        fastHash: true,
       },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `about-us-card`,
+        name: `about-us-cards`,
         path: `${__dirname}/content/about-us-cards`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `about-us-card`,
-        path: `${__dirname}/content/about-us-cards`,
+        fastHash: true,
       },
     },
     {
@@ -49,6 +36,7 @@ module.exports = {
       options: {
         name: `service-card`,
         path: `${__dirname}/content/service-card`,
+        fastHash: true,
       },
     },
     {
@@ -56,6 +44,7 @@ module.exports = {
       options: {
         name: `footer`,
         path: `${__dirname}/content/footer`,
+        fastHash: true,
       },
     },
     {
@@ -63,6 +52,7 @@ module.exports = {
       options: {
         name: `pricing-card`,
         path: `${__dirname}/content/pricing-card`,
+        fastHash: true,
       },
     },
     {
@@ -70,6 +60,7 @@ module.exports = {
       options: {
         name: `pricing-content`,
         path: `${__dirname}/content/pricing-content`,
+        fastHash: true,
       },
     },
     {
@@ -77,6 +68,7 @@ module.exports = {
       options: {
         name: `service-page`,
         path: `${__dirname}/content/service-page`,
+        fastHash: true,
       },
     },
     {
@@ -84,6 +76,7 @@ module.exports = {
       options: {
         name: `logo`,
         path: `${__dirname}/content/logo`,
+        fastHash: true,
       },
     },
     {
@@ -91,13 +84,15 @@ module.exports = {
       options: {
         name: `banner`,
         path: `${__dirname}/content/banner`,
+        fastHash: true,
       },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/static`,
+        name: `static-images`,
+        path: `${__dirname}/static/images`,
+        fastHash: true,
       },
     },
     `gatsby-plugin-netlify-cms`,
@@ -111,25 +106,199 @@ module.exports = {
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gatsby-icon.png`,
       },
     },
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
         fonts: [
-          `Nunito\:400,400i,700,700i`, // Specify font family and variants
+          `Nunito\:400,400i,700,700i`,
           `Open Sans\:400,400i,700,700i`,
           `Inter\:400,400i,700,700i`,
-          
         ],
-        display: "swap", // Optional: swap, block, fallback, optional
+        display: "swap",
       },
     },
-    `gatsby-plugin-styled-components`, // Add this line
+    `gatsby-plugin-styled-components`,
   ],
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// /**
+//  * Configure your Gatsby site with this file.
+//  *
+//  * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
+//  */
+
+// /**
+//  * @type {import('gatsby').GatsbyConfig}
+//  */
+// module.exports = {
+//   siteMetadata: {
+//     title: `Gatsby Default Starter`,
+//     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+//     author: `gatsbyjs`,
+//     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+//   },
+//   plugins: [
+//     `gatsby-plugin-image`,
+//     {
+//       resolve: `gatsby-source-filesystem`,
+//       options: {
+//         name: `images`,
+//         path: `${__dirname}/src/images`,
+//         fastHash: true,
+//       },
+//     },
+//     {
+//       resolve: `gatsby-source-filesystem`,
+//       options: {
+//         name: `about-us`,
+//         path: `${__dirname}/content/about-us`,
+//         fastHash: true,
+//       },
+//     },
+//     // {
+//     //   resolve: `gatsby-source-filesystem`,
+//     //   options: {
+//     //     name: `about-us-cards`,
+//     //     path: `${__dirname}/content/about-us-cards`,
+//     //     fastHash: true,
+//     //   },
+//     // },
+//     {
+//       resolve: `gatsby-source-filesystem`,
+//       options: {
+//         name: `service-card`,
+//         path: `${__dirname}/content/service-card`,
+//         fastHash: true,
+//       },
+//     },
+//     {
+//       resolve: `gatsby-source-filesystem`,
+//       options: {
+//         name: `footer`,
+//         path: `${__dirname}/content/footer`,
+//       },
+//     },
+//     {
+//       resolve: `gatsby-source-filesystem`,
+//       options: {
+//         name: `pricing-card`,
+//         path: `${__dirname}/content/pricing-card`,
+//       },
+//     },
+//     {
+//       resolve: `gatsby-source-filesystem`,
+//       options: {
+//         name: `pricing-content`,
+//         path: `${__dirname}/content/pricing-content`,
+//       },
+//     },
+//     {
+//       resolve: `gatsby-source-filesystem`,
+//       options: {
+//         name: `service-page`,
+//         path: `${__dirname}/content/service-page`,
+//       },
+//     },
+//     {
+//       resolve: `gatsby-source-filesystem`,
+//       options: {
+//         name: `logo`,
+//         path: `${__dirname}/content/logo`,
+//       },
+//     },
+//     {
+//       resolve: `gatsby-source-filesystem`,
+//       options: {
+//         name: `banner`,
+//         path: `${__dirname}/content/banner`,
+//       },
+//     },
+//     {
+//       resolve: `gatsby-source-filesystem`,
+//       options: {
+//         name: `images`,
+//         path: `${__dirname}/static/images`,
+//       },
+//     },
+//     `gatsby-plugin-netlify-cms`,
+//     `gatsby-transformer-remark`,
+//     `gatsby-transformer-sharp`,
+//     `gatsby-plugin-sharp`,
+//     {
+//       resolve: `gatsby-plugin-manifest`,
+//       options: {
+//         name: `gatsby-starter-default`,
+//         short_name: `starter`,
+//         start_url: `/`,
+//         background_color: `#663399`,
+//         // This will impact how browsers show your PWA/website
+//         // https://css-tricks.com/meta-theme-color-and-trickery/
+//         // theme_color: `#663399`,
+//         display: `minimal-ui`,
+//         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+//       },
+//     },
+//     {
+//       resolve: `gatsby-plugin-google-fonts`,
+//       options: {
+//         fonts: [
+//           `Nunito\:400,400i,700,700i`, // Specify font family and variants
+//           `Open Sans\:400,400i,700,700i`,
+//           `Inter\:400,400i,700,700i`,
+          
+//         ],
+//         display: "swap", // Optional: swap, block, fallback, optional
+//       },
+//     },
+//     `gatsby-plugin-styled-components`, // Add this line
+//   ],
+// }
